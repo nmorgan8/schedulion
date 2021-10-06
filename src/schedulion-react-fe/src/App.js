77dd6767ebdd27577af06e81c6aff202cdebce92
nmorgan8 <nmorgan8@lion.lmu.edu>
2021-10-06 15:02:20 -0700

    added multi page access

--- src/schedulion-react-fe/src/App.js
+++ src/schedulion-react-fe/src/App.js
@@ -3,20 +3,18 @@ import { BrowserRouter, Switch, Route, Link } from 'react-router-dom';
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
+export default function App() {
 
-  // added above code to test backend and as ex
+  // added code to test backend access
   const [currentTime, setCurrentTime] = useState(0);
+  const [currentLMUWin, setLMUWin] = useState(0);
+  const [currentLMULose, setLMULose] = useState(0);
 
   useEffect(() => {
     fetch('/api/time').then(res => res.json()).then(data => {
       setCurrentTime(data.time);
     });
   }, []);
-
-  // testing backend compatability
-  const [currentLMUWin, setLMUWin] = useState(0);
-  const [currentLMULose, setLMULose] = useState(0);
 
   useEffect(() => {
     fetch('/api/record').then(res => res.json()).then(data => {
@@ -25,39 +23,58 @@ function App() {
     })
   }, []);
 
+  const Home = () => (
+    <div>
+      <img src={logo} className="App-logo" alt="logo" />
+      <p>
+        Edit <code>src/App.js</code> and save to reload.
+      </p>
+      <a
+        className="App-link"
+        href="https://reactjs.org"
+        target="_blank"
+        rel="noopener noreferrer"
+        >
+        Learn React
+      </a>
+
+      {/* added for testing */}
+      <p>The current time is {currentTime}.</p>
+      <p>LMU Wins: {currentLMUWin}</p>
+      <p>LMU Loses: {currentLMULose}</p>
+    </div>
+  )
+
+  const Scheduling = () => (
+    <div>
+      <h2>Scheduling</h2>
+      <p>We will be adding various cool stuff to help with scheduling!</p>
+    </div>
+  );
+
+  const Statistics = () => (
+    <div>
+      <h2>Statistics</h2>
+      <p>UNLEASH YOUR INNER STATS NERD!!!</p>
+    </div>
+  )
+
+  // default application
   return (
     <div className="App">
       <header className="App-header">
         <BrowserRouter>
           <div>
-            <Link className="App-link" to="/">Home</Link>
+            <Link to="/">Home</Link>
             &nbsp;|&nbsp;
-            <Link className="App-link" to="/page2">Page2</Link>
+            <Link to="/scheduling">Scheduling</Link>
+            &nbsp;|&nbsp;
+            <Link to="/statistics">Statistics</Link>
           </div>
           <Switch>
-            <Route exact path="/">
-                <img src={logo} className="App-logo" alt="logo" />
-                <p>
-                  Edit <code>src/App.js</code> and save to reload.
-                </p>
-                <a
-                  className="App-link"
-                  href="https://reactjs.org"
-                  target="_blank"
-                  rel="noopener noreferrer"
-                >
-                  Learn React
-                </a>
-
-                {/* added for testing */}
-                <p>The current time is {currentTime}.</p>
-                <p>LMU Wins: {currentLMUWin}</p>
-                <p>LMU Loses: {currentLMULose}</p>
-
-            </Route>
-            <Route path="/page2">
-                <p>This is page 2!</p>
-            </Route>
+            <Route exact path="/"><Home /></Route>
+            <Route path="/scheduling"><Scheduling /></Route>
+            <Route path="/statistics"><Statistics /></Route>
           </Switch>
         </BrowserRouter>
       </header>
@@ -65,4 +82,4 @@ function App() {
   );
 }
 
-export default App;
+// export default App;
